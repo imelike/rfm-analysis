f89fcca842cc79006c837fb64d56c90d9ea09b45
imelike <melikeakalanmail@gmail.com>
2023-03-17 13:29:54 +0300

    first commit

---
+++ rfm.py
@@ -0,0 +1,148 @@
+###############################################################
+# RFM ile Müşteri Segmentasyonu (Customer Segmentation with RFM)
+###############################################################
+
+###############################################################
+# 1. İş Problemi (Business Problem)
+###############################################################
+
+# Değişkenler
+#
+# InvoiceNo: Fatura numarası. Her işleme yani faturaya ait eşsiz numara. C ile başlıyorsa iptal edilen işlem.
+# StockCode: Ürün kodu. Her bir ürün için eşsiz numara.
+# Description: Ürün ismi
+# Quantity: Ürün adedi. Faturalardaki ürünlerden kaçar tane satıldığını ifade etmektedir.
+# InvoiceDate: Fatura tarihi ve zamanı.
+# UnitPrice: Ürün fiyatı (Sterlin cinsinden)
+# CustomerID: Eşsiz müşteri numarası
+# Country: Ülke ismi. Müşterinin yaşadığı ülke.
+
+
+###############################################################
+# 2. Veriyi Anlama (Data Understanding)
+###############################################################
+
+import datetime as dt
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+df_ = pd.read_excel("online_retail_II.xlsx", sheet_name="Year 2009-2010")
+
+
+df = df_.copy()
+df.head()
+df.shape
+df.isnull().sum()
+
+# essiz urun sayisi nedir?
+df["Description"].nunique()
+
+df["Description"].value_counts().head()
+
+df.groupby("Description").agg({"Quantity": "sum"}).head()
+
+df.groupby("Description").agg({"Quantity": "sum"}).sort_values("Quantity", ascending=False).head()
+
+df["Invoice"].nunique()
+
+# Faturaki ürüne toplam kaç para ödenmiş
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+
+df.groupby("Invoice").agg({"TotalPrice": "sum"}).head()
+
+###############################################################
+# 3. Veri Hazırlama (Data Preparation)
+###############################################################
+
+df.shape
+df.isnull().sum()
+df.describe().T
+df.dropna(inplace=True)
+df = df[~df["Invoice"].astype(str).str.contains("C", na=False)]
+
+###############################################################
+# 4. RFM Metriklerinin Hesaplanması (Calculating RFM Metrics)
+###############################################################
+
+# Recency, Frequency, Monetary
+df.head()
+
+df["InvoiceDate"].max()
+
+today_date = dt.datetime(2010, 12, 11)
+type(today_date)
+
+rfm = df.groupby('Customer ID').agg({'InvoiceDate': lambda InvoiceDate: (today_date - InvoiceDate.max()).days,
+                                     'Invoice': lambda Invoice: Invoice.nunique(),
+                                     'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+rfm.head()
+
+rfm.columns = ['recency', 'frequency', 'monetary']
+
+rfm.describe().T
+
+rfm = rfm[rfm["monetary"] > 0]
+rfm.shape
+
+
+###############################################################
+# 5. RFM Skorlarının Hesaplanması (Calculating RFM Scores)
+###############################################################
+
+rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+
+# 0-100, 0-20, 20-40, 40-60, 60-80, 80-100
+
+rfm["frequency_score"] = pd.qcut(rfm['frequency'].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+
+rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
+
+rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
+                    rfm['frequency_score'].astype(str))
+
+rfm.describe().T
+
+rfm[rfm["RFM_SCORE"] == "55"]
+
+rfm[rfm["RFM_SCORE"] == "11"]
+
+###############################################################
+# 6. RFM Segmentlerinin Oluşturulması ve Analiz Edilmesi (Creating & Analysing RFM Segments)
+###############################################################
+# regex
+
+# RFM isimlendirmesi
+seg_map = {
+    r'[1-2][1-2]': 'hibernating',
+    r'[1-2][3-4]': 'at_Risk',
+    r'[1-2]5': 'cant_loose',
+    r'3[1-2]': 'about_to_sleep',
+    r'33': 'need_attention',
+    r'[3-4][4-5]': 'loyal_customers',
+    r'41': 'promising',
+    r'51': 'new_customers',
+    r'[4-5][2-3]': 'potential_loyalists',
+    r'5[4-5]': 'champions'
+}
+
+rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
+
+# Hangi segmentte kaç müşteri var?
+rfm[["segment", "recency", "frequency", "monetary"]].groupby("segment").agg(["mean", "count"])
+
+rfm[rfm["segment"] == "cant_loose"].head()
+rfm[rfm["segment"] == "cant_loose"].index
+
+new_df = pd.DataFrame()
+new_df["new_customer_id"] = rfm[rfm["segment"] == "new_customers"].index
+
+new_df["new_customer_id"] = new_df["new_customer_id"].astype(int)
+
+# new_df.to_csv("new_customers.csv")
+# rfm.to_csv("rfm.csv")
+
+
+
+
